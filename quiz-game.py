2c7a09203725428a8cdc93e00f613c481a6713de
durgauri <minochndk@gmail.com>
2020-08-07 15:41:07 +0700

    first

---
+++ quiz-game.py
@@ -0,0 +1,97 @@
+print("GAME QUIZ ISLAMI\n")
+print("Siapakah pendiri masjid Al-Aqsa?")
+print("a. Nabi Isa\nb. Nabi Nuh\nc. Nabi Muhammad\nd. Nabi Yakub")
+
+while True:
+    response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+
+    if response == "d":
+        break
+    
+    else:
+        print("\nJawaban anda kurang tepat, coba lagi")
+        while True:
+            response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+            if response == "d":
+                stop = True
+                break
+            else:
+                print("\nJawaban anda kurang tepat, coba lagi")
+                stop = False
+                break
+
+        if stop:
+            break
+
+print("Siapakah sahabat Rasulullah yang mendapat gelar pedang Allah terhunus?")
+print("a. Khalid bin Walid\nb. Abdullah bin Umi Maktum\nc. Mushab bin Umeir\nd. Sa'ad bin Abi Waqqas")        
+while True:
+    response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+
+    if response == "a":
+        break
+    
+    else:
+        print("\nJawaban anda kurang tepat, coba lagi")
+        while True:
+            response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+            if response == "a":
+                stop = True
+                break
+            else:
+                print("\nJawaban anda kurang tepat, coba lagi")
+                stop = False
+                break
+
+        if stop:
+            break
+
+print("\nBerapakah jumlah tentara Islam dalam Perang Uhud?")
+print("a. 300 orang\nb. 700 orang\nc. 800 orang\nd. 900 orang\n")
+while True:
+    response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+
+    if response == "b":
+        break
+    
+    else:
+        print("\nJawaban anda kurang tepat, coba lagi")
+        while True:
+            response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+            if response == "b":
+                stop = True
+                break
+            else:
+                print("\nJawaban anda kurang tepat, coba lagi")
+                stop = False
+                break
+
+        if stop:
+            break
+
+
+print("Berapa lama masa Utsman bin Affan menjabat sebagai khalifah?")
+print("a. 12 th\nb. 13 th\nc. 14 th\nd. 15 th")
+while True:
+    response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+
+    if response == "a":
+        break
+    
+    else:
+        print("\nJawaban anda kurang tepat, coba lagi")
+        while True:
+            response = input("Silahkan pilih jawaban anda a, b, c, atau d\n")
+            if response == "a":
+                stop = True
+                break
+            else:
+                print("\nJawaban anda kurang tepat, coba lagi")
+                stop = False
+                break
+
+        if stop:
+            break
+        
+
+    
